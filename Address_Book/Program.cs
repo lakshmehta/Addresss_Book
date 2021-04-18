b109f99b7e85bb628cab24bd6f2767db73c6853c
unknown <lakshmehta06@gmail.com>
2021-04-18 17:02:39 +0530

    [Laksh added Addcontacts]

--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Address_Book
 {
     class Program
     {
+        public static Dictionary<string, List<AddContact>> addressBookStore = new Dictionary<string, List<AddContact>>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to the Address Book");
+
+
+            Console.WriteLine("Enter the book name you want to store");
+            string bookName = Console.ReadLine();
+            AddressBookMain.addBook(bookName);
         }
     }
 }
